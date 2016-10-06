d4a4f4923d96f71d7611ddead3447b50d42159dd
Trevor Livingston <tlivings@gmail.com>
2016-10-05 21:21:31 -0500

    using passthrough stream

--- test/test.js
+++ test/test.js
@@ -6,15 +6,6 @@ const Util = require('util');
 
 Test('logger', (t) => {
 
-    t.test('create', (t) => {
-        t.plan(2);
-
-        const logger = Logging.createLogger('test');
-
-        t.ok(Util.isObject(logger), 'returned object.');
-        t.ok(Util.isFunction(logger.log), 'has log function.');
-    });
-
     t.test('subscribe and unsubscribe', (t) => {
         t.plan(6);
 
@@ -22,41 +13,24 @@ Test('logger', (t) => {
 
         const logger = Logging.createLogger('test-subscribe');
 
-        const listener = ({ source, name, timestamp, tags, data }) => {
-            t.equal(source, 'log-emit', 'source is correct.');
-            t.equal(name, 'test-subscribe', 'name is correct.');
-            t.ok(Util.isNumber(timestamp), 'timestamp is a number.');
-            t.ok(Util.isArray(tags), 'tags is an array.');
-            t.equal(data, 'hello world', 'data is correct.');
-        };
-
-        Logging.subscribe(listener);
-
-        logger.log('hello world');
-
-        Logging.unsubscribe(listener);
+        const subscription = Logging.subscribe(
+            ({ source, name, timestamp, tags, data }) => {
+                logged++;
+                t.equal(source, 'log-emit', 'source is correct.');
+                t.equal(name, 'test-subscribe', 'name is correct.');
+                t.ok(Util.isNumber(timestamp), 'timestamp is a number.');
+                t.ok(Util.isArray(tags), 'tags is an array.');
+                t.equal(data, 'hello world', 'data is correct.');
+            }
+        );
 
         logger.log('hello world');
 
-        t.equal(logged, 0, 'did not log again.');
-    });
-
-    t.test('unsubscribeAll', (t) => {
-        t.plan(1);
-
-        let logged = 0;
-
-        const logger = Logging.createLogger('test-unsubscribeAll');
-
-        Logging.subscribe(({ source, name, timestamp, tags, data }) => {
-            ++logged;
-        });
-
-        Logging.unsubscribeAll();
+        subscription.unsubscribe();
 
         logger.log('hello world');
 
-        t.equal(logged, 0, 'did not log.');
+        t.equal(logged, 1, 'did not log again.');
     });
 
 });
